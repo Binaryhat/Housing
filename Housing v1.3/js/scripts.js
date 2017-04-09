f0d45ddf40ecb15a145fe73ea8ea2f79cb7ff992
binaryhat <binaryhat18@gmail.com>
2017-04-09 13:37:06 +0530

    detail page footer completed

--- Housing v1.3/js/scripts.js
+++ Housing v1.3/js/scripts.js
@@ -10,12 +10,15 @@ Version      : 1.0
 (function ($) {
 	"use strict";
 
-
 	
 	var navOffset = $(".detail-nav").offset().top;
 
 	var secondNavFixedPos= $("nav.navbar").height() + 10;
 
+
+	/*==============================================================================
+	                      =          01.stiky side and subnav           =
+	=================================================================================*/
 
 	$(window).scroll(function(){
 		var scrollpos = $(window).scrollTop();
@@ -28,4 +31,45 @@ Version      : 1.0
 			
 		}
 	});
+
+	/*==============================================================================
+	                      =          02. Bank offers slider           =
+	=================================================================================*/
+	$('.bank-slider').slick({
+	    dots: false,
+	     infinite: false,
+	     speed: 300,
+	     slidesToShow: 5,
+	     slidesToScroll: 1,
+	     
+	     responsive: [
+	       {
+	         breakpoint: 1024,
+	         settings: {
+	           slidesToShow: 3,
+	           slidesToScroll: 3,
+	           infinite: true,
+	           dots: false
+	         }
+	       },
+	       {
+	         breakpoint: 600,
+	         settings: {
+	           slidesToShow: 2,
+	           slidesToScroll: 2
+	         }
+	       },
+	       {
+	         breakpoint: 480,
+	         settings: {
+	           slidesToShow: 1,
+	           slidesToScroll: 1
+	         }
+	       }
+	       // You can unslick at a given breakpoint now by adding:
+	       // settings: "unslick"
+	       // instead of a settings object
+	     ]
+	});
+
 })(jQuery);
